1ac9c39cd1f1b71c79ae4eba103bf952c6df9b8c
joey57 <njorogejoey5@gmail.com>
2022-04-22 14:32:10 +0300

    create init method and credentials list

--- credential.py
+++ credential.py
@@ -2,3 +2,12 @@ class Credentials:
   '''
   Class that generates new instances of passwords
   '''
+  credentials_list= []
+
+  def __init__(self, login_credentials):
+    '''
+    __init__ method that helps us define properties for our objects
+    '''
+    self.login_credentials = login_credentials
+
+      
