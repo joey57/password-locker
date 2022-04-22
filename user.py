edb407c1d45483ce2866266123236b6465b0f708
joey57 <njorogejoey5@gmail.com>
2022-04-22 13:14:43 +0300

    create empty user array

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ class User:
   '''
   Class that generates new instances of users
   '''
+  user_detail = []
+  
   def __init__(self, login_username):
     '''
     _inti_ method that helps us define properties for our objects
