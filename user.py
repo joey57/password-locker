3e6e18464466cbb50cad59233b36f4823ba5173a
joey57 <njorogejoey5@gmail.com>
2022-04-22 13:20:33 +0300

    add user variable

--- user.py
+++ user.py
@@ -4,11 +4,12 @@ class User:
   '''
   user_detail = []
   
-  def __init__(self, login_username):
+  def __init__(self, login_account, login_username):
     '''
     _inti_ method that helps us define properties for our objects
     '''
     self.login_username = login_username
+    self.login_account = login_account
 
     
         
