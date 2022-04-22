9c892760c92ba302b4a160e526cea33b205e6a37
joey57 <njorogejoey5@gmail.com>
2022-04-22 13:19:15 +0300

    import unittest and user class

---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for the user class behaviors
+
+  Args:
+     unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+  
