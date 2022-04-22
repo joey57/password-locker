e734487e7636536c47f1807811e6b02611bd69b7
joey57 <njorogejoey5@gmail.com>
2022-04-22 13:14:05 +0300

    create user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+  '''
+  Class that generates new instances of users
+  '''
+  def __init__(self, login_username):
+    '''
+    _inti_ method that helps us define properties for our objects
+    '''
+    self.login_username = login_username
+
+    
+        
