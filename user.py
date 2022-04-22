589ae81b9c3495d12f2ec030a39f66cab215ef0d
joey57 <njorogejoey5@gmail.com>
2022-04-22 13:45:42 +0300

    add save-detail method

--- user.py
+++ user.py
@@ -5,6 +5,12 @@ class User:
   # empty list where user accounts will be created
   user_detail = []
   
+  def save_detail(self):
+    '''
+    save-detail method saves user object into user-detail list
+    '''
+    User.user_detail.append(self)
+  
   def __init__(self, login_account, login_username):
     '''
     _inti_ method that helps us define properties for our objects
