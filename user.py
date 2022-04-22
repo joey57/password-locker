70614916d469efa58165afa241c95013dc5ce9a0
joey57 <njorogejoey5@gmail.com>
2022-04-22 14:00:41 +0300

    4th test passed added delete_detail method

--- user.py
+++ user.py
@@ -18,5 +18,11 @@ class User:
     self.login_account = login_account
     self.login_username = login_username
 
+  def delete_detail(self):
+    '''
+    delete_detail method deletes a saved user detail from user detail list
+    '''  
+    User.user_detail.remove(self)
+
     
         
