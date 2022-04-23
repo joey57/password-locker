cddd72b651da8892f81ded46725f945eee2228ba
joey57 <njorogejoey5@gmail.com>
2022-04-23 13:55:49 +0300

    added generate random password

--- run.py
+++ run.py
@@ -1,136 +1,225 @@
-from getpass import getpass
-import random
+#!/usr/bin/env python3.6
 from user import User
-from credential import Credentials
-dash = '-'*60
+import getpass
+import random
+import string
+dash = '-' * 60
 
-def create_account(account_name, username, password, confirm_password):
-  '''
-  function to create an account
-  '''
-  new_user = User(account_name, username, password,confirm_password)
-  return new_user
+def create_account(account_name,username,password,confirm_password):
 
-def save_details():
-  '''
-  function to save details
-  '''
-  User.save_detail()
+    """
+    function to create a new account
+    """
+
+    new_user = User(account_name,username,password,confirm_password)
+
+    return new_user
+
+def save_details(user):
+
+    """
+    function to save save_details
+    """
+    user.save_detail()
 
 def display_all_details():
-  '''
-  function to display the saved details
-  '''
-  return User.display_all_details()
+
+    """
+    function used to return all saved save_details
+    """
+    return User.display_all_details()
 
 def check_existing_user(username):
-  '''
-  function to check and return existing accounts
-  '''
-  return User.user_exist(username)
+
+    """
+    a function that is used to check and return all exissting accounts
+    """
+
+    return User.user_exist(username)
 
 def find_user(username):
-  '''
-  function to check details from save_details
-  '''
-  return User.find_by_username(username)
+
+    """
+    the function is used check details from the saved save_details
+    """
+
+    return User.find_by_username(username)
 
 def generatePassword(num):
-  genpas = ''
-  for n in range(num):
-    x = random.randint(0,94)
-  return genpas  
+   genpas = ''
+
+   for n in range(num):
+       x = random.randint(0,94)
+       genpas += string.printable[x]
+
+   return genpas
 
 def main():
-  print("Welcome this is password locker!")
-  print('\n')
-  print("login")
-  print("\n")
+    print('{:_^5}'.format('WELCOME TO PASSWORD LOCKER!'))
 
-  print("What is your username?")
-  user_name = input()
 
-  print(f"Hello {user_name}, welcome to password locker, what would you like to do")
-  print('\n')
+    print('\n')
 
-  while True:
-    print("Use the following short codes: 1- register a new account, 2- display account, 3- find contacts, 4-exit password locker")
+    print('{:_^20}'.format('login'))
 
-    short_code = input().lower()
+    print('\n')
 
-    if short_code == '1':
-      print(f"{user_name} please fill int the following")
+    print("Please enter your username")
 
-      print("First name")
-      f_name = input()
-      print("-"*10)
-      print("account name")
-      account_name = input()
+    user_name = input().upper()
 
-      print("username")
-      username = input()
-      
+    print(f"Hello {user_name}, what do you want to do?")
 
-      print("Do you want a randomly generated password?")
-      ans = input()
+    print('\n')
 
-      password = getpass.getpass('password:')
-      print("*****")
-      confirm_password = getpass.getpass('confirm password:')
-      print("*****")
-      save_details(create_account(account_name,password,confirm_password)) 
-      print('\n')
-      print(f"{user_name} {account_name} account of {username} created and password save")
-      print('\n')
+    while True:
 
-      print(f"{user_name} what else do you want to do")
+        list =('''
+        1-Register a new account
+        2-Display accounts
+        3-Find accounts
+        4-Exit the locker\n''')
+        print(list)
 
-    elif short_code == '2':
-      if display_all_details():
-        print(f"{user_name} here is a list of all your accounts")
-        print('\n')
-        for user in display_all_details():
-          print(f"{user.account_name} {user.username}.......{user.password}")
-          print('\n')
-      else:
-        print('\n')
-        print(f"{user_name} you dont seem to have any account saved yet")
-        print('\n')
 
-    elif short_code == '3': 
-      print(f"Enter the username you want to search for")
-      search_username = input()
-      if check_existing_user(search_username):
-        search_username = find_user(search_username) 
 
-        print(dash)
+        short_code = input().lower()
 
-        print(f"Account is {search_username.account_name}.com")
-        print(f"Account username is {search_username.username}")
-        print(f"Account password is {search_username.password} dont give out passwords")
+        if short_code == '1':
 
-        print(dash)
+            print(f"{user_name} Please fill the following")
 
-        print(f"{user_name} what else do you want to do?")
+            print("-"*10)
 
-      else:
-        print(f"{user_name} That account does not exist")
+            print ("Account name")
 
-        print(f"{user_name} what else do you want to do?")
+            account_name = input()
 
-    elif short_code == "4":
-  
-        print("Bye .......")
+            print('\n')
 
-        break
+            print("Username")
+            username = input()
 
-    else:
-        print("I really didn't get that. Please use the correct code")
+            print('\n')
 
-        print(f"{user_name} what else do you want to do?")
-          
-if __name__ == "__main__":
-  main()          
+            print("Do you want a randomly generated password?")
+
+
+            print("yes", "no")
+            ans = input().lower()
+
+            if ans == 'yes':
+
+                genpas = print(generatePassword(10))
+
+                save_details(create_account(account_name,username,password,confirm_password))
+
+                print ('\n')
+
+
+                print(f"{user_name} {account_name} account of {username} created and password saved")
+
+                print ('\n')
+
+
+            elif ans == 'no':
+                password = getpass.getpass('password:')
+                print("*********")
+
+                confirm_password = getpass.getpass('confirm password:')
+                print("*********")
+
+                save_details(create_account(account_name,username,password,confirm_password))
+
+                print ('\n')
+
+                print(dash)
+
+                print(f"Hey {user_name}")
+                print(f"Your account name is {account_name}.com")
+                print(f"Your account username is {username}")
+                print("passwords encripted but can be viwed using option 2 / 3")
+
+                print(dash)
+
+                print ('\n')
+
+                print(f"{user_name} what else do you want to do?")
+
+        elif short_code =='2':
+
+
+            if display_all_details():
+
+                print(f"{user_name} here is a list of all your saved accounts")
+
+                print('\n')
+
+                for user in display_all_details():
+
+                    print(dash)
+
+                    print(f"Account is {user.account_name}.com")
+                    print(f"Account username is {user.username}")
+                    print(f"The account's password is {user.password}")
+
+                    print(dash)
+
+                    print('\n')
+
+                    print(f"{user_name} what else do you want to do?")
+
+            else:
+
+                print('\n')
+
+                print(f" {user_name} You dont seem to have any contacts saved yet")
+
+                print('\n')
+
+                print(f"{user_name} what else do you want to do?")
+
+        elif short_code == '3':
+
+            print(f"{user_name} Enter a username you want to search for")
+
+            search_username = input()
+
+
+            if check_existing_user(search_username):
+
+                search_username = find_user(search_username)
+
+                print(dash)
+
+                print(f"Account is {search_username.account_name}.com")
+                print(f"Account username is {search_username.username}")
+                print(f"Account password is {search_username.password}")
+
+                print(dash)
+
+                print(f"{user_name} what else do you want to do?")
+
+            else:
+                print(f"{user_name} That account does not exist")
+
+                print(f"{user_name} what else do you want to do?")
+
+        elif short_code == "4":
+
+            print("Bye .......")
+
+            break
+
+        else:
+            print("I really didn't get that. Please use the correct code")
+
+            print(f"{user_name} what else do you want to do?")
+
+
+if __name__ == '__main__':
+
+    main()                 
       
 
   
