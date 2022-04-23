49ac4b64c6866bd852749e28e8406f1a72714921
joey57 <njorogejoey5@gmail.com>
2022-04-23 12:19:59 +0300

    added save and display functions

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 from user import User
 from credential import Credentials
 
+def create_account(account_name, username, password, confirm_password):
+  '''
+  function to create an account
+  '''
+  new_user = User(account_name, username, password,confirm_password)
+  return new_user
+
+def save_details(user):
+  '''
+  function to save details
+  '''
+  user.save_detail()
+
+def display_all_details():
+  '''
+  function to display the saved details
+  '''
+  return User.display_all_details()
+
